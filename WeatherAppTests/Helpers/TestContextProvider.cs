b544d76dda93e29df41e34ed25d925edbb161793
Jarda Jonas <jarda000@seznam.cz>
2023-03-09 14:28:51 +0100

    some comments added

--- WeatherAppTests/Helpers/TestContextProvider.cs
+++ WeatherAppTests/Helpers/TestContextProvider.cs
@@ -7,11 +7,16 @@ namespace WeatherAppTests.Helpers
 {
     public class TestContextProvider
     {
+        // Creates a new instance of ApplicationDbContext using an in-memory SQLite database.
         public static ApplicationDbContext CreateContextFromScratch()
         {
             var connection = new SqliteConnection("Datasource=:memory:");
             connection.Open();
+
+            // Create a new instance of DbContextOptions using the SQLite connection.
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(connection).Options;
+
+            // Create a new instance of ApplicationDbContext using the DbContextOptions.
             var context = new ApplicationDbContext(options);
 
             context.Database.EnsureCreated();
@@ -21,13 +26,19 @@ namespace WeatherAppTests.Helpers
 
         public static ApplicationDbContext CreateContextFromFactory(CustomWebAppFactory factory)
         {
+            // Create a new service scope for the factory.
             var scope = factory.Services.CreateScope();
+
+            // Get the ApplicationDbContext instance from the service scope.
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
+            // Get the underlying database connection from the ApplicationDbContext instance.
             var connection = context.Database.GetDbConnection();
+
             connection.Close();
             connection.Open();
 
+            // Ensure that the database has been deleted and re-created.
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
 
